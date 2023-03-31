26ba7c306567277fb6c9e169a15b7f9e6b6841a5
hrishi12345 <51530702+hrishi12345@users.noreply.github.com>
2023-03-31 14:15:06 +0530

    added functionality

--- my-app/src/NewExpense/ExpenseForm.js
+++ my-app/src/NewExpense/ExpenseForm.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const [enteredTitle,setitle]=useState('')
+  const [enteredAmount,setAmount]=useState('')
+  const [enteredDate,setDate]=useState('')
+  const TitleChange=(event)=>{
+     setitle(event.target.value)
+  }
+  const AmountChange=(event)=>{
+    setAmount(event.target.value)
+    
+ }
+ const DateChange=(event)=>{
+  setDate(event.target.value)
+  
+}
+  const send=(event)=>{
+    event.preventDefault()
+    const data={
+      title:enteredTitle,
+      amount:enteredAmount,
+      date:enteredDate
+    }
+    console.log(data)
+    setitle('')
+    setAmount('')
+    setDate('')
+    
+  }
   return (
     <form>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' />
+          <input type='text' onChange={TitleChange} value={enteredTitle}/>
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input type='number' min='0.01' step='0.01' onChange={AmountChange} value={enteredAmount}/>
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' />
+          <input type='date' min='2019-01-01' max='2022-12-31' onChange={DateChange} value={enteredDate}/>
         </div>
       </div>
       <div className='new-expense__actions'>
-        <button type='submit'>Add Expense</button>
+        <button type='submit' onClick={send}>Add Expense</button>
       </div>
     </form>
   );
