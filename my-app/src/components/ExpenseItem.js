86de7e6fcf0cbfb35af3fc433eacbb1080f6ed1f
hrishi12345 <51530702+hrishi12345@users.noreply.github.com>
2023-03-31 10:39:42 +0530

    Update ExpenseItem.js
    delete button

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,14 +1,24 @@
+import React, { useRef } from 'react';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
-import ExpenseDate  from './ExpenseDate'; 
-import ExpenseDetails from './ExpenseDetail';
-function ExpenseItem(props) {
 
+const ExpenseItem = (props) => {
+  const cardRef = useRef(null);
+
+  const deleteButtonHandler = () => {
+    cardRef.current.parentNode.removeChild(cardRef.current);
+  }
 
   return (
-    <div className='expense-item'>
-     <ExpenseDate  date={props.date}/>
-      <ExpenseDetails title={props.title} amount={props.amount} />
-    </div>
+    <Card className='expense-item' ref={cardRef}>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+        <button onClick={deleteButtonHandler}>Delete</button>
+      </div>
+    </Card>
   );
 }
 
